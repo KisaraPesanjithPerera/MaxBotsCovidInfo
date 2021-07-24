2f98d3731b466753a93d30066a43e3efbfea8466
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-07-23 21:14:34 -0700

    Update CoronaBot.py

--- CoronaBot.py
+++ CoronaBot.py
@@ -15,7 +15,7 @@ def staat(qq):
   data = {
     "chat_id": str(qq),
     "photo": "https://telegra.ph/file/444f0805649cfcdf19112.jpg",
-    "caption": "ශ්‍රී ලංකාවේ කොරෝනා තතු එසැනින් දැනගන්න. @SLCovid19slbzonebot Group එකට Add කරගත් පසු ස්වයංක්‍රියව නවතම කොරෝනා තතු ලබාගත හැක.  වැඩි විස්තර සදහා /help භාවිතා කරන්න.     ~ @sl_bot_zone ",
+    "caption": "Get instant access to Corona in Sri Lanka.  Automatically retrieve the latest corona information after adding it to the @SLCovid19slbzonebot Group. Use /help for more information. @sl_bot_zone ",
     "parse_mode": "HTML",
     "reply_markup": {
         "inline_keyboard": [
@@ -53,31 +53,31 @@ def staa():
 
     textt = str(
                     '<b>CURRENT SITUATION</b>' + '\n' + '\n' + '<b>' +
-                    update_date_time + ' වන විට</b>' + '\n' + '\n' +
-                    '<b>🇱🇰 ශ්‍රී ලංකාවේ තත්ත්වය</b>' + '\n' + '\n'  +
-                    '🤒 තහවුරු කරනලද රෝගීන් සංඛ්‍යාව(සමුච්චිත) = ' + '<code>' +
+                    update_date_time + ' now </b>' + '\n' + '\n' +
+                    '<b>🇱🇰 Situation in Sri Lanka</b>' + '\n' + '\n'  +
+                    '🤒 Number of confirmed patients (cumulative) = ' + '<code>' +
                     local_total_cases + '</code>' + '\n' +
-                    '🤕 ප්‍රතිකාර ලබන රෝගීන් සංඛ්‍යාව = ' + '<code>' + local_active_cases + '</code>' +
-                    '\n' + '😷 නව රෝගීන් සංඛ්‍යාව = ' + '<code>' + local_new_cases + '</code>' +
+                    '🤕 Number of patients receiving treatment = ' + '<code>' + local_active_cases + '</code>' +
+                    '\n' + '😷 Number of new patients = ' + '<code>' + local_new_cases + '</code>' +
                     '\n' +
-                    '🏥 දැනට රෝහල්වල විමර්ශන යටතේ සිටින පුද්ගලයින් = ' + '<code>' +
+                    '🏥 Persons currently under investigation in hospitals = ' + '<code>' +
                     local_total_number_of_individuals_in_hospitals +  '</code>' + '\n' +
-                    '🙂 සුවය ලබා පිටව ගිය සංඛ්‍යාව = ' + '<code>' + local_recovered + '</code>' + 
-                    '\n' + '⚰ මරණ සංඛ්‍යාව = ' + '<code>'  + local_deaths + '</code>' + '\n' +
-                    '\n' + '<b>🌎 ලොව පුරා තත්ත්වය</b>' + '\n' +
-                    '\n' + '🤒 තහවුරු කරනලද රෝගීන් සංඛ්‍යාව (සමුච්චිත) = ' '<code>'  +
-                    global_total_cases + '</code>' + '\n' + '😷 නව රෝගීන් සංඛ්‍යාව = ' '<code>'  +
-                    global_new_cases + '</code>' + '\n' + '⚰ මරණ සංඛ්‍යාව = ' '<code>'  +
-                    global_deaths + '</code>' + '\n' + '🙂 සුවය ලැබූ සංඛ්‍යාව = ' '<code>'  +
+                    '🙂 The number of people who have recovered and left = ' + '<code>' + local_recovered + '</code>' + 
+                    '\n' + '⚰ Number of deaths = ' + '<code>'  + local_deaths + '</code>' + '\n' +
+                    '\n' + '<b>🌎 Worldwide status</b>' + '\n' +
+                    '\n' + '🤒 Number of confirmed patients (cumulative) = ' '<code>'  +
+                    global_total_cases + '</code>' + '\n' + '😷 Number of new patients = ' '<code>'  +
+                    global_new_cases + '</code>' + '\n' + '⚰ Number of deaths = ' '<code>'  +
+                    global_deaths + '</code>' + '\n' + '🙂 Healed number = ' '<code>'  +
                     global_recovered + '</code>' + '\n' + '\n' + '\n' +
-                    '✅ සියලු තොරතුරු රජයේ සහ පිලිගත් මුලාශ්‍ර මගිනි' + '\n' +
+                    '✅ All information is provided by the government and reputable sources' + '\n' +
                     '~ @sl_bot_zone 🇱🇰 ~')
     return textt
 
 
 def sta():
          r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
-         reply_text = f"**රට {r['country']} 🦠**\n🤒 තහවුරු කරනලද රෝගීන් සංඛ්‍යාව(සමුච්චිත) = {r['cases']:,}\n😷 නව රෝගීන් සංඛ්‍යාව = {r['todayCases']:,}\n⚰ මරණ සංඛ්‍යාව = {r['deaths']:,}\n⚰ නව මරණ සංඛ්‍යාව = {r['todayDeaths']:,}\n🙂 සුවය ලැබූ සංඛ්‍යාව =  {r['recovered']}"
+         reply_text = f"**රට {r['country']} 🦠**\n🤒 Number of confirmed patients (cumulative) = {r['cases']:,}\n😷 Number of new patients= {r['todayCases']:,}\n⚰ Number of deaths = {r['deaths']:,}\n⚰ New death toll= {r['todayDeaths']:,}\n🙂 Healed number =  {r['recovered']}"
          message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 
@@ -100,7 +100,7 @@ async def corona(event):
 
 @bot.on(events.NewMessage(pattern='/help'))
 async def help(event):
-    await event.respond('නවතම කොරෝනා ප්‍රවෘත්ති බැලීමට /corona command භාවිතා කරන්න ')
+    await event.respond('Use the /corona command to view the latest corona news ')
     raise events.StopPropagation
 
 def main():
