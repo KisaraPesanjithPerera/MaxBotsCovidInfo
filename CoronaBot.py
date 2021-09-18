831838e09716a82f25183116f98c1690aabaf05e
KisaraPesanjithPerera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-09-18 11:40:04 +0530

    Update CoronaBot.py

--- CoronaBot.py
+++ CoronaBot.py
@@ -15,7 +15,7 @@ def staat(qq):
   data = {
     "chat_id": str(qq),
     "photo": "https://telegra.ph/file/444f0805649cfcdf19112.jpg",
-    "caption": "Get instant access to Corona in Sri Lanka 📊 .  Automatically retrieve the latest corona information after adding it to the @SLCovid19slbzonebot Group 🦠 . Use /help for more information. @sl_bot_zone ",
+    "caption": "**Get instant access to Corona in Sri Lanka 📊 . I'm automatically retrieve the latest corona informations 🦠 . Use /help for more information. @SweetAngelUpdates**",
     "parse_mode": "HTML",
     "reply_markup": {
         "inline_keyboard": [
